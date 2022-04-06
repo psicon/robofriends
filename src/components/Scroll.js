f6df149e89a697dd224ecf3858357d094bca97a3
psicon <psieber@psicon.de>
2022-04-06 11:06:18 +0200

    pages

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Scroll = (props) => {
-    console.log(props);
     return (
         <div style={{overflowY: 'scroll', border: '1px solid black', height: '800px'}}>
             {props.children}
